0dfcb643d4b20d13a82b7b8d6cc3a9605fd8e91e
ponsianodeloor <ponsianodeloor@gmail.com>
2024-07-24 13:51:23 -0500

    modulos y paginas para formularios reactivos

---
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-page',
+  templateUrl: './basic-page.component.html',
+  styleUrl: './basic-page.component.css'
+})
+export class BasicPageComponent {
+
+}
