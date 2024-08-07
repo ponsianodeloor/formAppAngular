1c6c42358dcd8a2e7d68801cfb53bb127df9a067
Ponsiano De Loor <pdeloor@sudamericano.fin.ec>
2024-08-06 23:34:50 -0500

    uo de forms para usar check y option

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-switches-page',
   templateUrl: './switches-page.component.html',
   styleUrl: './switches-page.component.css'
 })
-export class SwitchesPageComponent {
+export class SwitchesPageComponent implements OnInit {
+
+  public myForm:FormGroup = new FormGroup({});
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.myForm = this.fb.group({
+      gender: ['M', Validators.required],
+      notifications: [true, Validators.required],
+      terms: [false, Validators.required],
+    });
+  }
+
+  save() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    const formValue = this.myForm.value;
+    console.log(formValue);
+  }
 
 }
